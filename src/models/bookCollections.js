087fc7eb31ebaeeec8e0cfac7b9614eef9addade
25pal <pallaviuttekar333@gmail.com>
2022-12-19 23:29:04 +0530

    mongo-2

---
+++ src/models/bookCollections.js
@@ -0,0 +1,34 @@
+
+const mongoose=require('mongoose')
+ 
+const bookSchema= new mongoose.Schema({
+   bookName: {
+       type: String,
+       required: true
+   },
+   ISBN: {
+       type: String,
+       required: true,
+       unique: true
+   },
+   author: String,
+   tags: [ String ], //array of strings
+   year: Number,
+   isPublished: {
+       type: Boolean, //Boolean
+       default: false
+   },
+   prices: {
+       indianPrice: String,
+       europeanPrice: String,
+       japanPrice: String
+   },
+   sales: {
+       type: Number,
+       default : 0
+   },
+   completionDate: String
+ 
+}, {timestamps: true} )
+ 
+module.exports = mongoose.model( 'Book', bookSchema )
