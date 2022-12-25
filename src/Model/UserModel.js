8b8bf8d1e9dbd8afec857e01c647e26dd1a8647d
25pal <pallaviuttekar333@gmail.com>
2022-12-25 11:50:19 +0530

    free

---
+++ src/Model/UserModel.js
@@ -0,0 +1,33 @@
+//const { default: mongoose } = require('mongoose');
+const mongoose=require('mongoose');
+const userSchema=new mongoose.Schema({
+        name:String,
+        balance:{
+            type:Number,
+            default:100
+        },
+        address:String,
+        age:Number,
+        gender:{
+            type:String,
+            enum:["Male","Female","Other"]
+        },
+        isFreeAppUser:{
+            type:Boolean,
+            default:false
+        }
+
+
+})
+
+module.exports=mongoose.model('user',userSchema);
+
+// { 
+// 	_id: ObjectId("61951bfa4d9fe0d34da86829"),
+// 	name: "Sabiha Khan",
+// 	balance:100, // Default balance at user registration is 100
+// 	address:"New delhi",
+// 	age: 90,
+//  	gender: “female” // Allowed values are - “male”, “female”, “other”
+// 	isFreeAppUser: false // Default false value.
+// }
